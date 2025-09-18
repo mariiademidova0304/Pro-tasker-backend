7a48ab1a0ee5b0312142c57f83e1b099816c0ec9
Mariia D <mariia.demidova0304@gmail.com>
2025-09-18 14:31:16 -0400

    Added cors

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const app = express();
 const db = require('./config/connection');
 const routes = require('./routes/index');
-
+const cors = require('cors');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.use('/', routes);
 
